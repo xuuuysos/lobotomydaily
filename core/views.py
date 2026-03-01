64351d5866177c950588499e05e09ec45acda976
Ivan <juteex9@gmail.com>
2026-03-01 18:46:46 +0300

    I created a framework for the main page and for the registration field.

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def register(request):
+    return render(request, 'core/register.html')
