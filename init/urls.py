d7f4d41ca54c343278e5412f1ff647beba89919b
Denis Kazak <yaspeedyperfectman@gmail.com>
2026-03-01 15:56:50 +0000

    Edited urls and added urls of register, login, accounts

--- init/urls.py
+++ init/urls.py
@@ -6,5 +6,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('profile', views.profile),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('register/', views.register, name='register'),
 ]
