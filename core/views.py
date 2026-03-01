5a19394b7328b8ea3e17bd3d60e43ab0c32bd81f
Denis Kazak <yaspeedyperfectman@gmail.com>
2026-03-01 15:53:25 +0000

    Edited views and added registration, profile, login

--- core/views.py
+++ core/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+def get_general_context(request):
+    """
+    Создает общий контекст
+    """
+    context = {
+        'user': request.user,
+        'menu': [
+            ['Main page', '/'],
+            ['Create new news', '/create_news'],
+        ]
+    }
 
+    if request.user.is_authenticated:
+        context['menu'].append(['Profile', '/profile'])
+    else:
+        context['menu'].append(['Login', '/accounts/login'])
+        context['menu'].append(['Registration', '/accounts/register'])
+    return context
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+def profile(request):
+    user = request.user
+    context = {
+        'user': user
+    }
+    context.update(get_general_context(request))
+    return render(request, "profile.html", context)
 
 def index(request):
     return render(request, 'core/index.html')
-
-
-def register(request):
-    return render(request, 'core/register.html')
