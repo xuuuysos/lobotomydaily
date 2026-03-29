6c9a21f2eeec08f408f9a461a235855009e36ace
Sigma <okak228@gmail.com>
2026-03-29 18:04:25 +0300

    add testing

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your tests here.
+
+class IndexPage(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get('/')
+
+    def test_index(self):
+        self.assertEqual(self.response.status_code, 200)
+
+   
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='vasya', password='testpassword')
+    
+    def test_index_response(self):
+        profile = self.client.get("/profile")
+        self.assertEqual(profile.status_code, 404) 
+        self.client.force_login(self.user)
+        profile_logged_in = self.client.get("/profile")
+        self.assertEqual(profile_logged_in.status_code, 200)
