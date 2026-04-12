a6b722d6296155ad22ab377b94d78ebfc57c973a
Ivan <juteex9@gmail.com>
2026-04-12 19:37:21 +0300

    Merge remote-tracking branch 'origin/main'

--- init/urls.py
+++ init/urls.py
@@ -5,7 +5,7 @@ import core.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', core.views.index, name='index'),
-    path('profile', core.views.profile),
+    path('profile/', core.views.profile),
     path('register/', core.views.register, name='register'),
     path('api/category-news/', core.views.fetch_category_news, name='fetch_category_news'),
     path('api/article-body/', core.views.fetch_article_body, name='fetch_article_body'),
